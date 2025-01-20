4901acbf20ccfa901fe89a22f1611357bc103b15
yashArtoon <yash.bhakhar@artoon.in>
2025-01-20 16:55:17 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Enable static exports
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  basePath: "/<repository-name>",
-};
+const nextConfig = {};
 
 export default nextConfig;
