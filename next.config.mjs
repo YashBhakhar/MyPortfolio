d60dfe3917ca5fc30610982d5d9f0da5108ec342
YashBhakhar <bhakharyash44@gmail.com>
2025-09-18 19:46:22 +0530

    nodemailer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverRuntimeConfig: {
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_SECURE: process.env.SMTP_SECURE,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    TO_EMAIL: process.env.TO_EMAIL,
+  },
+};
 
 export default nextConfig;
